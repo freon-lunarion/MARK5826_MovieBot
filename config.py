b8e3d7dc63bd1035f6f0c69d02f811c9902f66c3
freon-lunarion <antonio.daniswara@gmail.com>
2019-06-27 09:10:32 +1000

    reupload config

--- config.py
+++ config.py
@@ -23,9 +23,6 @@ SLACK_BOT_NAME = 'Monday'
 # SLACK_BOT_TOKEN='xoxb-xxxxxxxxxxxx-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx'
 # SLACK_VERIFICATION_TOKEN='xxxxxxxxxxxxxxxxxxxxxxxx'
 
-SLACK_BOT_TOKEN='xoxb-597388589376-638279594800-x3mSKl67mY14dTFNHrhE21P2'
-SLACK_VERIFICATION_TOKEN='3ftQdW22Cmwq2XeSkEnqfusj'
-
 # instantiate Slack client
 slack_client = SlackClient(SLACK_BOT_TOKEN) # do not change this parameter
 
@@ -40,14 +37,6 @@ slack_client = SlackClient(SLACK_BOT_TOKEN) # do not change this parameter
 # )
 
 # workspace_id = 'xxxxxxxxxxxxxxxxxxxxxxxx' # replace with Assistant ID
-
-service = ibm_watson.AssistantV1(
-    iam_apikey = '0B2HITIOQodvrG9zEnBsFBZQHteLtpYBEqS2KECTCMkV', # replace with Password
-    url = 'https://gateway-syd.watsonplatform.net/assistant/api', # replace with Watson Assistant's Credentials - URL
-    version = '2018-09-20'
-)
-
-workspace_id = 'cf182008-1628-4a21-8b4b-87b20a2de328' # replace with Assistant ID
 
 ###################################################################
 ######## Log files configuration   ##########################
