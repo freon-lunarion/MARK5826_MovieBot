d000df801899fd75c852edaa0f0a5d1053aa6c8e
freon-lunarion <antonio.daniswara@gmail.com>
2019-06-27 14:48:01 +1000

    blank config

--- config.py
+++ config.py
@@ -18,10 +18,10 @@ location = os.path.dirname(os.path.realpath(__file__)) + '/'
 ###################################################################
 ######## Slack configuration   ##########################
 ###################################################################
-SLACK_BOT_NAME = 'Monday'
+SLACK_BOT_NAME = 'Your_Bot_Name_HERE'
 
-# SLACK_BOT_TOKEN='xoxb-xxxxxxxxxxxx-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx'
-# SLACK_VERIFICATION_TOKEN='xxxxxxxxxxxxxxxxxxxxxxxx'
+SLACK_BOT_TOKEN='xoxb-xxxxxxxxxxxx-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx' # copy from "Install App" -> "Bot User OAuth Access Token" 
+SLACK_VERIFICATION_TOKEN='xxxxxxxxxxxxxxxxxxxxxxxx' # copy from "Basic Infromation" -> "App Credential" -> "Verication Token"
 
 # instantiate Slack client
 slack_client = SlackClient(SLACK_BOT_TOKEN) # do not change this parameter
@@ -30,13 +30,13 @@ slack_client = SlackClient(SLACK_BOT_TOKEN) # do not change this parameter
 ######## Watson service configuration   ##########################
 ###################################################################
 
-# service = ibm_watson.AssistantV1(
-#     iam_apikey = 'xxxxxxxxxxxxxxxxxxxxxxxx', # replace with Password
-#     url = 'https://xxxxxxxxxx.watsonplatform.net/assistant/api', # replace with Watson Assistant's Credentials - URL
-#     version = '2018-09-20'
-# )
+service = ibm_watson.AssistantV1(
+    iam_apikey = 'xxxxxxxxxxxxxxxxxxxxxxxx', # replace with Password/ API KEY
+    url = 'https://xxxxxxxxxx.watsonplatform.net/assistant/api', # replace with Watson Assistant's Credentials - URL
+    version = '2018-09-20'
+)
 
-# workspace_id = 'xxxxxxxxxxxxxxxxxxxxxxxx' # replace with Assistant ID
+workspace_id = 'xxxxxxxxxxxxxxxxxxxxxxxx' # replace with Worksapce ID
 
 ###################################################################
 ######## Log files configuration   ##########################
